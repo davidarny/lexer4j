42c22b0d42b1ea2523c3038a717817934c49f6d0
David Arutiunian <arutunjan666@mail.com>
2020-01-21 00:13:13 +0300

    chore: refactor TokenType

--- src/main/java/lexer/TokenType.java
+++ src/main/java/lexer/TokenType.java
@@ -5,45 +5,45 @@ package lexer;
  * language
  */
 public enum TokenType {
-    BlockComment,
-    LineComment,
-    WhiteSpace,
-    Tab,
-    NewLine,
-    CloseBrace,
-    OpenBrace,
-    OpeningCurlyBrace,
-    ClosingCurlyBrace,
-    DoubleConstant,
-    IntConstant,
-    Plus,
-    Minus,
-    Multiply,
-    Divide,
-    Point,
-    EqualEqual,
-    Equal,
-    NotEqual,
-    Greater,
-    Less,
-    Static,
-    Public,
-    Private,
-    Int,
-    Double,
-    Void,
-    False,
-    True,
-    Null,
-    Return,
-    New,
-    Class,
-    If,
-    While,
-    Else,
-    Semicolon,
-    Comma,
-    Identifier;
+    BLOCK_COMMENT,
+    LINE_COMMENT,
+    WHITE_SPACE,
+    TAB,
+    NEW_LINE,
+    CLOSE_BRACE,
+    OPEN_BRACE,
+    OPENING_CURLY_BRACE,
+    CLOSING_CURLY_BRACE,
+    DOUBLE_CONSTANT,
+    INT_CONSTANT,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    POINT,
+    EQUAL_EQUAL,
+    EQUAL,
+    NOT_EQUAL,
+    GREATER,
+    LESS,
+    STATIC,
+    PUBLIC,
+    PRIVATE,
+    INT,
+    DOUBLE,
+    VOID,
+    FALSE,
+    TRUE,
+    NULL,
+    RETURN,
+    NEW,
+    CLASS,
+    IF,
+    WHILE,
+    ELSE,
+    SEMICOLON,
+    COMMA,
+    IDENTIFIER;
 
     /**
      * Determines if this token is auxiliary
@@ -51,10 +51,10 @@ public enum TokenType {
      * @return {@code true} if token is auxiliary, {@code false} otherwise
      */
     public boolean isAuxiliary() {
-        return this == BlockComment ||
-            this == LineComment ||
-            this == NewLine ||
-            this == Tab ||
-            this == WhiteSpace;
+        return this == BLOCK_COMMENT ||
+            this == LINE_COMMENT ||
+            this == NEW_LINE ||
+            this == TAB ||
+            this == WHITE_SPACE;
     }
 }
